3fc5405d445dc433cc8e48090c2b9d4a70bdf764
fabian <fabian@einzelmensch.de>
2025-02-27 22:22:28 +0100

    switch to canvas theme

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,16 @@
 export default defineNuxtConfig({
-  // https://github.com/nuxt-themes/alpine
+  colorMode: {
+    preference: "dark",
+    fallback: "dark",
+  },
   extends: "@nuxt-themes/alpine",
   content: {
+    renderer: {
+      anchorLinks: false,
+    },
     preview: {
       api: "https://api.nuxt.studio",
+      dev: true,
       gitInfo: {
         name: "RNGesus/alpine",
         owner: "RNGesus",
@@ -19,4 +26,90 @@ export default defineNuxtConfig({
     "@nuxt/eslint",
     "@nuxt/content",
   ],
+
+  mdc: {
+    highlight: {
+      theme: {
+        dark: "github-dark",
+        default: "github-dark",
+        light: "github-light",
+      },
+    },
+  },
+
+  runtimeConfig: {
+    public: {
+      resend: !!process.env.NUXT_PRIVATE_RESEND_API_KEY,
+    },
+  },
+
+  routeRules: {
+    // Needed to activate preview on Nuxt Studio
+    "/": { prerender: false },
+  },
+
+  future: {
+    compatibilityVersion: 4,
+  },
+
+  experimental: {
+    viewTransition: true,
+  },
+
+  compatibilityDate: "2025-01-05",
+
+  nitro: {
+    experimental: {
+      websocket: true,
+    },
+    prerender: {
+      autoSubfolderIndex: false,
+      crawlLinks: true,
+      routes: ["/en", "/fr"],
+    },
+  },
+
+  hooks: {
+    "nitro:config": (config) => {
+      if (process.env.NUXT_PRIVATE_RESEND_API_KEY) {
+        config.handlers?.push({
+          method: "post",
+          route: "/api/emails/send",
+          handler: "~~/server/emails/send.ts",
+        });
+      }
+    },
+  },
+  i18n: {
+    locales: [
+      { code: "en", name: "English", language: "en-US" },
+      { code: "fr", name: "French", language: "fr-FR" },
+    ],
+    detectBrowserLanguage: {
+      useCookie: true,
+      cookieKey: "i18n_redirected",
+      redirectOn: "root",
+    },
+    strategy: "prefix",
+    defaultLocale: "en",
+    vueI18n: "~/i18n.config.ts",
+  },
+
+  icon: {
+    customCollections: [
+      {
+        prefix: "custom",
+        dir: "./app/assets/icons",
+      },
+    ],
+    clientBundle: {
+      scan: true,
+      includeCustomCollections: true,
+    },
+    provider: "iconify",
+  },
+
+  ogImage: {
+    zeroRuntime: true,
+  },
 });
