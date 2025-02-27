32edcef64656c481da411854fd6208ff1c7ce70f
fabian <fabian@einzelmensch.de>
2025-02-27 22:10:59 +0100

    update configs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,22 @@
 export default defineNuxtConfig({
   // https://github.com/nuxt-themes/alpine
-  extends: '@nuxt-themes/alpine',
-
-  modules: [// https://github.com/nuxt-modules/plausible
-  // https://github.com/nuxt/devtools
-  '@nuxtjs/plausible', '@nuxt/devtools', '@nuxt/eslint', '@nuxt/content']
-})
+  extends: "@nuxt-themes/alpine",
+  content: {
+    preview: {
+      api: "https://api.nuxt.studio",
+      gitInfo: {
+        name: "RNGesus/alpine",
+        owner: "RNGesus",
+        url: "https://github.com/RNGesus/alpine",
+      },
+    },
+  },
+  modules: [
+    // https://github.com/nuxt-modules/plausible
+    // https://github.com/nuxt/devtools
+    "@nuxtjs/plausible",
+    "@nuxt/devtools",
+    "@nuxt/eslint",
+    "@nuxt/content",
+  ],
+});
